2f6a2eb025e5bfe5fdb595999d74de12c9490801
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2018-10-07 16:22:38 -0400

    Add h1 to main

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ var pageBody = document.getElementsByTagName('body')[0];
 var scriptTag = document.getElementsByTagName('script')[0];
 pageBody.insertBefore(main, scriptTag);
 
+var header = document.createElement('h1');
+main.appendChild(header);
+
