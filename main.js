4928540ea35bc63033ead8e298b4612d24ab86f2
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2018-10-07 16:18:53 -0400

    Update main.js to insert tags

---
+++ main.js
@@ -0,0 +1,5 @@
+var main = document.createElement('main');
+var pageBody = document.getElementsByTagName('body')[0];
+var scriptTag = document.getElementsByTagName('script')[0];
+pageBody.insertBefore(main, scriptTag);
+
